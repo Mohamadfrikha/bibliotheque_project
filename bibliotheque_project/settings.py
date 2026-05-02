c44658926162c61d713ea50c24b91882f96db4dd
Mohamed Frikha <mohamedfrikha65@gmail.com>
2026-05-02 10:06:29 +0200

    update

--- bibliotheque_project/settings.py
+++ bibliotheque_project/settings.py
@@ -78,9 +78,15 @@ WSGI_APPLICATION = 'bibliotheque_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+   'default': {
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('POSTGRES_DB'),
+        'USER': config('POSTGRES_USER'),
+        'PASSWORD': config('POSTGRES_PASSWORD'),
+        'HOST': config('POSTGRES_HOST', default='db'),
+        'PORT': '5432',
     }
 }
 
